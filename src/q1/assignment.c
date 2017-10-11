5a554a8f8d945cd8586a9fe1c555e09d7f4ab4b1
Naoise Callery <naoisecallery@gmail.com>
2017-10-11 14:21:44 +0100

    Question 1
    Code for question 1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -2,5 +2,18 @@
 
 int main(int arg, char* argc[]){
   printf("Hello assignment1.\n");
+
+  int a = 1;
+  int* b = &a;
+  long c = 1000000;
+  double* d;
+  char** e;
+
+  printf("int a = %d\n", sizeof(a));
+  printf("int* b = %p\n", sizeof(b));
+  printf("long c = %lld\n", sizeof(c));
+  printf("double* d = %p\n", sizeof(d));
+  printf("char** e = %p\n", sizeof(e));
+
   return 0;
 }
